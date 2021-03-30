d18508bb653a9dbf17df16481063a0d775910b57
Victor Raony <victor_raony12@hotmail.com>
2021-03-30 13:45:52 -0400

    Exluindo e atualizando instrutores

--- instructors.js
+++ instructors.js
@@ -76,3 +76,44 @@ exports.edit = function(req, res){
 
     return res.render('instructors/edit', {instructor })
 }
+
+exports.put = function(req, res){
+    const {id} = req.body 
+    let index = 0
+    const foundInstructor = data.instructors.find(function(instrutor, foundindex){
+        if (id == instrutor.id){
+            index = foundindex
+            return true
+        }
+})
+    if (!foundInstructor) return res.send("Instructor not found!")
+    const instructor = {
+        ...foundInstructor,
+        ...req.body,
+        birth: Date.parse(req.body.birth)
+    }
+
+    data.instructors[index] = instructor
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+        if(err) return res.send("Write error")
+
+        return res.redirect(`/instructors/${id}`)
+    })
+}
+
+exports.delete = function(req, res){
+    const { id } = req.body
+
+    const filteredInstructors = data.instructors.filter(function(instructor){
+        return instructor.id != id
+    })
+
+    data.instructors = filteredInstructors
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+        if (err) return res.send("Write file error")
+
+        return res.redirect("/instructors")
+    })
+}
